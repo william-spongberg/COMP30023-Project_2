a8704df4f6c3e0fab00e9a2234d05e7b31b5eb3b
Lewis Zhang <liang.lewis.z@gmail.com>
2024-04-29 19:59:54 +1000

    preliminary implementation of mime module, have comments to indicate next tasks: loop the searching prodecure until end of email body

--- src/mime.c
+++ src/mime.c
@@ -1,5 +1,86 @@
-//
-// Created by liang on 4/27/2024.
-//
-
 #include "mime.h"
+
+int fetch_email_body(const int *clinet_socket_fd, const int msg_num) {
+    // fetch email body
+    char *fetch_cmd = construct_retrieve_cmd(&msg_num);
+
+    int byte_send = send(*clinet_socket_fd, fetch_cmd, strlen(fetch_cmd), 0);
+
+    if (byte_send == -1) {
+        perror("send");
+        fprintf(stderr, "Failed to send fetch command\n");
+        return -1;
+    }
+
+    if (byte_send == 0) {
+        fprintf(stderr, "Connection closed\n");
+        return -1;
+    }
+
+    free(fetch_cmd);
+
+    return 0;
+}
+
+int get_mime(const int *clinet_socket_fd, const int msg_num) {
+    // fetch email body
+    int fetch_status = fetch_email_body(clinet_socket_fd, msg_num);
+    if (fetch_status == -1) {
+        fprintf(stderr, "Failed to fetch email body\n");
+        return -1;
+    }
+
+    // Receive response from server
+    char response[MAX_DATA_SIZE];
+    int recv_len;
+    recv_len = recv(*clinet_socket_fd, response, MAX_DATA_SIZE, 0); // Read all the response
+
+    // Setup required content type for search
+    char *content_type = "Content-Type: text/plain; charset=UTF-8";
+    char content_transfer_encoding[][] = {"quoted-printable", "7bit", "8bit"};
+
+    // Boundary to separate the body parts
+    char *boundary_header = "boundary=";
+    char *boundary_ptr = strstr(response, boundary);
+    // Extract the boundary value
+    char *boundary_start = boundary_ptr + strlen(boundary_header) + 1;
+    char *boundary_end = strchr(boundary_start, '\"');
+    char *boundary_value = (char *)malloc(boundary_end - boundary_start + 1);
+    strncpy(boundary_value, boundary_start, boundary_end - boundary_start);
+    // Add "--" to search for the boundary in the email body
+    char *boundary = (char *)malloc(strlen(boundary_value) + 3);
+    boundary = strcat("--", boundary_value);
+
+    // FIXME: the following procedure need to be looped to find until the end of the email
+    // Search for the content type
+    char *content_type_ptr = strstr(response, content_type);
+
+    if (content_type_ptr == NULL) {
+        fprintf(stderr, "Content type text/plain with UTF-8 charset not found\n");
+        return -1;
+    }
+
+    // From the occurrence of matching content type found, search for matching content transfer encoding
+    for (int i = 0; i < 3; i++) {
+        char *content_transfer_encoding_ptr = strstr(content_type_ptr, content_transfer_encoding[i]);
+        if (content_transfer_encoding_ptr != NULL) {
+            // Found matching transfer encoding, print the body part separated by "--boundary"
+            char *body_end = strstr(content_transfer_encoding_ptr, boundary);
+            char *body_start = content_transfer_encoding_ptr;
+            char *body = (char *)malloc(body_end - body_start + 1);
+            strncpy(body, body_start, body_end - body_start);
+            printf("%s\n", body);
+
+            // Free the allocated memory
+            free(content_type_ptr);
+            free(boundary);
+            free(body);
+            return 0;
+        }
+    }
+
+    // TODO: Repeat above procedure until the end of the email
+    // while()
+
+    return -1;
+}
--- src/mime.h
+++ src/mime.h
@@ -1,4 +1,34 @@
 #ifndef MIME_H
 #define MIME_H
 
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "retrieve.h"
+
+#define MIME_TAG "A04"
+#define MIME_TAG_SIZE 3
+#define MIME_CMD "FETCH"
+#define MIME_CMD_SIZE 5
+#define MIME_BODY "BODY[]"
+#define MIME_BODY_SIZE 6
+
+// GOAL: find the first UTF-8 text/plain body part of an email
+
+// Fetch body of email
+
+// identify MIME header
+    // Version (Always 1.0 for this project)
+    // Content type (Always text/plain for this project) ; boundary (AKA delimiter)
+
+// Within the body, find the parts separated by the boundary and satisfy all of the following condition:
+    // Content type is text/plain
+    // Charset is UTF - 8
+    // Content transfer encoding quoted-printable | 7bit | 8bit
+
+int get_mime(const int *client_socket_fd, char *msg_num);
 #endif
