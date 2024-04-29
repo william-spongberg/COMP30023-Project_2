16d135f88d9e2ae01089cfbec1ae68bbaf2c14e7
William Spongberg <wspongberg@student.unimelb.edu.au>
2024-04-29 13:51:53 +1000

    Refactor main.c to extract get_message function and parse_headers function to parse.c

--- src/main.c
+++ src/main.c
@@ -16,7 +16,6 @@
 void get_tag(char *buffer, size_t size);
 void send_command(char *tag, char *command, char *line, char **buffer,
                   int connfd, FILE *stream);
-char *get_message(char *header);
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -104,19 +103,7 @@ int main(int argc, char *argv[]) {
     char *fetch =
         " FETCH 1 BODY.PEEK[HEADER.FIELDS (FROM TO DATE SUBJECT)]\r\n";
     send_command(tag, fetch, line, &buffer, connfd, stream);
-
-    // get headers from first occurence of header title
-    char *from = strstr(buffer, "From: ");
-    char *to = strstr(buffer, "To: ");
-    char *date = strstr(buffer, "Date: ");
-    char *subject = strstr(buffer, "Subject: ");
-
-    // print headers or empty string or error message
-    printf("From: %s\n", get_message(from) ? get_message(from) : "");
-    printf("To: %s\n", get_message(to) ? get_message(to) : "");
-    printf("Date: %s\n", get_message(date) ? get_message(date) : "");
-    printf("Subject: %s\n", get_message(subject) ? get_message(subject) : "<No subject>");
-
+    parse_headers(buffer);
     memset(buffer, 0, sizeof(buffer));
 
     // free memory
@@ -127,18 +114,6 @@ int main(int argc, char *argv[]) {
         close(connfd);
     }
     return 0;
-}
-
-char *get_message(char *header) {
-    // remove first word
-    char *start = strchr(header, ' ') + 1;
-    // remove trailing chars after message
-    char *end = strchr(header, '\r');
-    // copy message
-    char *message = (char *)malloc(end - start + 1);
-    strncpy(message, start, end - start);
-    message[end - start] = '\0';
-    return message;
 }
 
 // generate a unique tag that is used to identify each command
--- src/parse.h
+++ src/parse.h
@@ -5,4 +5,7 @@
 #ifndef PARSE_H
 #define PARSE_H
 
+void parse_headers(char *buffer);
+char *get_message(char *header);
+
 #endif
