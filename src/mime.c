11ce711f85039d3d2f4e3c1a1f79b305fbaf4efa
William Spongberg <william@spongberg.dev>
2024-05-06 14:29:40 +1000

    Updated mime.c to use retrieve.c methods, added old working mime code as reference

--- src/mime.c
+++ src/mime.c
@@ -1,40 +1,12 @@
 #include "mime.h"
-#define MAX_DATA_SIZE 4096
+#include "memory.h"
+#include "retrieve.h"
 
-int fetch_email_body(const int *clinet_socket_fd, const int msg_num) {
-    // fetch email body
-    char *fetch_cmd = construct_retrieve_cmd(&msg_num);
-
-    int byte_send = send(*clinet_socket_fd, fetch_cmd, strlen(fetch_cmd), 0);
-
-    if (byte_send == -1) {
-        perror("send");
-        fprintf(stderr, "Failed to send fetch command\n");
-        return -1;
-    }
-
-    if (byte_send == 0) {
-        fprintf(stderr, "Connection closed\n");
-        return -1;
-    }
-
-    free(fetch_cmd);
-
-    return 0;
-}
-
-int get_mime(const int *clinet_socket_fd, const int msg_num) {
-    // fetch email body
-    int fetch_status = fetch_email_body(clinet_socket_fd, msg_num);
-    if (fetch_status == -1) {
-        fprintf(stderr, "Failed to fetch email body\n");
-        return -1;
-    }
-
-    // Receive response from server
-    char response[MAX_DATA_SIZE];
-
-    int recv_len = recv(*clinet_socket_fd, response, MAX_DATA_SIZE, 0); // Read all the response
+int get_mime(char *str_message_num, char **tag, char **buffer, int connfd,
+             FILE *stream) {
+    // retrieve email body
+    retrieve_body(str_message_num, tag, buffer, connfd, stream);
+    char *body = *buffer;
 
     // Setup required content type for search
     char *content_type = "Content-Type: text/plain; charset=UTF-8";
@@ -42,30 +14,36 @@ int get_mime(const int *clinet_socket_fd, const int msg_num) {
 
     // Boundary to separate the body parts
     char *boundary_header = "boundary=";
-    char *boundary_ptr = strstr(response, boundary_header);
+    char *boundary_ptr = strstr(body, boundary_header);
     // Extract the boundary value
     char *boundary_start = boundary_ptr + strlen(boundary_header) + 1;
     char *boundary_end = strchr(boundary_start, '\"');
     char *boundary_value = (char *)malloc(boundary_end - boundary_start + 1);
     strncpy(boundary_value, boundary_start, boundary_end - boundary_start);
     // Add "--" to search for the boundary in the email body
-    char *boundary = (char *)malloc(strlen(boundary_value) + 3);
+    char *boundary = (char *)malloc(strlen(boundary_value) + 5);
+    check_memory(boundary);
     boundary = strcat("--", boundary_value);
 
-    char *content_type_ptr = strstr(response, content_type);
+    char *content_type_ptr = strstr(body, content_type);
 
     if (content_type_ptr == NULL) {
-        fprintf(stderr, "Content type text/plain with UTF-8 charset not found\n");
+        fprintf(stderr,
+                "Content type text/plain with UTF-8 charset not found\n");
         return -1;
     }
 
-    while(content_type_ptr != NULL) {
-        // From the occurrence of matching content type found, search for matching content transfer encoding
+    while (content_type_ptr != NULL) {
+        // From the occurrence of matching content type found, search for
+        // matching content transfer encoding
         for (int i = 0; i < 3; i++) {
-            char *content_transfer_encoding_ptr = strstr(content_type_ptr, content_transfer_encoding[i]);
+            char *content_transfer_encoding_ptr =
+                strstr(content_type_ptr, content_transfer_encoding[i]);
             if (content_transfer_encoding_ptr != NULL) {
-                // Found matching transfer encoding, print the body part separated by "--boundary"
-                char *body_end = strstr(content_transfer_encoding_ptr, boundary);
+                // Found matching transfer encoding, print the body part
+                // separated by "--boundary"
+                char *body_end =
+                    strstr(content_transfer_encoding_ptr, boundary);
                 char *body_start = content_transfer_encoding_ptr;
                 char *body = (char *)malloc(body_end - body_start + 1);
                 strncpy(body, body_start, body_end - body_start);
@@ -77,7 +55,8 @@ int get_mime(const int *clinet_socket_fd, const int msg_num) {
                 return 0;
             }
 
-            // If no matching content transfer encoding found, search for the next content type
+            // If no matching content transfer encoding found, search for the
+            // next content type
             content_type_ptr = strstr(content_type_ptr + 1, content_type);
         }
     }
@@ -85,6 +64,56 @@ int get_mime(const int *clinet_socket_fd, const int msg_num) {
     // Free the allocated memory
     free(content_type_ptr);
 
-    // No required content type with the desired charset and transferencoding found
+    // No required content type with the desired charset and transferencoding
+    // found
     return -1;
+}
+
+// TODO: old working version of mime, fix function above with this
+void parse_mime(char *buffer) {
+    // match mime boundary
+    char *tmp = strstr(buffer, "boundary=");
+    if (tmp == NULL) {
+        fprintf(stderr, "No boundary found\n");
+        exit(4);
+    }
+
+    // get boundary
+    char *start = strchr(tmp, '=') + 1;
+    char *end = strchr(tmp, '\r');
+    tmp = (char *)malloc(end - start + 1);
+    strncpy(tmp, start, end - start);
+    tmp[end - start] = '\0';
+
+    char *boundary = (char *)malloc(strlen(tmp) + 3);
+    strcpy(boundary, "--");
+    strcat(boundary, tmp);
+    printf("Boundary: %s\n", boundary);
+
+    // get headers using position given by boundary
+    char *hdr = strstr(buffer, boundary);
+    start = strstr(hdr, "Content-Transfer-Encoding: ");
+    start = strchr(start, ' ') + 1;
+    end = strchr(start, '\r');
+    char *encoding = (char *)malloc(end - start + 1);
+    strncpy(encoding, start, end - start);
+    encoding[end - start] = '\0';
+    printf("Content-Transfer-Encoding: %s\n", encoding);
+    
+    start = strstr(hdr, "Content-Type: ");
+    start = strchr(start, ' ') + 1;
+    end = strchr(start, '\r');
+    encoding = (char *)malloc(end - start + 1);
+    strncpy(encoding, start, end - start);
+    encoding[end - start] = '\0';
+    printf("Content-Type: %s\n", encoding);
+
+    start = strstr(hdr, "\r\n\r\n");
+    start += 4;
+    end = strstr(start, boundary);
+    char *message = (char *)malloc(end - start + 1);
+    strncpy(message, start, end - start);
+    message[end - start] = '\0';
+    printf("[Start Message]%s", message);
+    printf("[End Message]\n\n");
 }
--- src/mime.h
+++ src/mime.h
@@ -1,20 +1,8 @@
 #ifndef MIME_H
 #define MIME_H
 
-#include <netdb.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include "retrieve.h"
-
-#define MIME_TAG "A04"
-#define MIME_TAG_SIZE 3
-#define MIME_CMD "FETCH"
-#define MIME_CMD_SIZE 5
-#define MIME_BODY "BODY[]"
-#define MIME_BODY_SIZE 6
 
 // GOAL: find the first UTF-8 text/plain body part of an email
 
@@ -29,5 +17,6 @@
     // Charset is UTF - 8
     // Content transfer encoding quoted-printable | 7bit | 8bit
 
-int get_mime(const int *client_socket_fd, const int msg_num);
+int get_mime(char *str_message_num, char **tag, char **buffer, int connfd,
+             FILE *stream);
 #endif
