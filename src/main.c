d826068f359d5e028246a5e0f5b14a92c5eed028
William Spongberg <william@spongberg.dev>
2024-05-06 14:30:45 +1000

    Refactored main.c with new simplified methods

--- src/main.c
+++ src/main.c
@@ -1,14 +1,13 @@
+#include "command.h"
+#include "connect.h"
 #include "login.h"
+#include "memory.h"
+#include "mime.h"
 #include "parse.h"
 #include "read.h"
 #include "tag.h"
-#include "command.h"
-#include "memory.h"
-#include "connect.h"
-#include "mime.h"
+#include "retrieve.h"
 
-#define LOGIN "LOGIN"
-#define SELECT "SELECT"
 #define FETCH "FETCH"
 #define BODY "BODY.PEEK[]"
 #define BODY_HEADERS "BODY.PEEK[HEADER.FIELDS (FROM TO DATE SUBJECT)]"
@@ -28,7 +27,7 @@ int main(int argc, char *argv[]) {
     int_message_num = atoi(str_message_num);
 
     // print command line arguments for debugging
-    printf("Username: %s\n", username);
+    printf("\nUsername: %s\n", username);
     printf("Password: %s\n", password);
     printf("Folder: %s\n", folder);
     printf("Message number: %d\n", int_message_num);
@@ -41,14 +40,16 @@ int main(int argc, char *argv[]) {
     // initialise stream
     FILE *stream = fdopen(connfd, "r+");
 
-    // initialise buffer
+    // initialise buffer + tag
     char *buffer = (char *)calloc(MAX_DATA_SIZE, sizeof(char));
     check_memory(buffer);
     char *tag = (char *)calloc(MAX_TAG_SIZE, sizeof(char));
     check_memory(tag);
     get_num_tag(tag, MAX_TAG_SIZE);
-    login(&connfd, username, password);
-    select_folder(&connfd, folder, &int_message_num);
+
+    // login and select folder
+    login(username, password, &tag, &buffer, connfd, stream);
+    select_folder(folder, &tag, &buffer, connfd, stream);
 
     /**
      * TODO: Proposed: instead of sending command in main, let each module send
@@ -60,43 +61,33 @@ int main(int argc, char *argv[]) {
 
     // commands
     if (strcmp(command, "retrieve") == 0) {
-        printf("|Retrieve command|\n");
-        // retrieve command
-        char *body = create_command(3, FETCH, str_message_num, BODY);
-        send_command(&tag, body, &buffer, connfd, stream);
+        printf("[retrieve]\n");
+        retrieve_body(str_message_num, &tag, &buffer, connfd, stream);
         // print response
-        printf("Received: %s\n", buffer);
+        printf("Received:\n%s\n", buffer);
         printf("\n");
         memset(buffer, 0, MAX_DATA_SIZE);
-        free(body);
     } else if (strcmp(command, "parse") == 0) {
-        printf("|Parse command|\n");
-        // parse command
-        char *headers = create_command(3, FETCH, str_message_num, BODY_HEADERS);
-        send_command(&tag, headers, &buffer, connfd, stream);
-        parse_headers(buffer);
+        printf("[parse]\n");
+        parse_headers(str_message_num, &tag, &buffer, connfd, stream);
         memset(buffer, 0, MAX_DATA_SIZE);
-        free(headers);
     } else if (strcmp(command, "mime") == 0) {
-        printf("|MIME command|\n");
-        // mime command
-        char *body = create_command(3, FETCH, str_message_num, BODY);
-        send_command(&tag, body, &buffer, connfd, stream);
-        // TODO: fix args given to get_mime
-        //get_mime(buffer);
+        printf("[mime]\n");
+        // TODO: fix mime
+        get_mime(str_message_num, &tag, &buffer, connfd, stream);
         memset(buffer, 0, MAX_DATA_SIZE);
-        free(body);
     } else if (strcmp(command, "list") == 0) {
-        printf("|List command|\n");
-        // TODO: fix list command not returning anything
-        // list command
-        char *list = create_command(1, "UID FETCH 1:* (UID)");
-        send_command(&tag, list, &buffer, connfd, stream);
+        printf("[list]\n");
+        // TODO: fix fetch command not returning anything
+        // TODO: use list.c methods
+        // fetch command
+        char *fetch_command = create_command(1, "UID FETCH 1:* (UID)");
+        send_command(fetch_command, &tag, &buffer, connfd, stream);
         // print response
-        printf("Received: %s\n", buffer);
+        printf("Received:\n%s\n", buffer);
         printf("\n");
         memset(buffer, 0, MAX_DATA_SIZE);
-        free(list);
+        free(fetch_command);
     } else {
         fprintf(stderr, "Invalid command\n");
         exit(1);
