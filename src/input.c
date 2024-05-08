02391cc41229bc27a647191839f4796f3d4f41ad
William Spongberg <wspongberg@student.unimelb.edu.au>
2024-05-08 21:09:45 +1000

    Fixed not ending lines correctly in input.c

--- src/input.c
+++ src/input.c
@@ -40,11 +40,11 @@ void read_command_line(int argc, char *argv[], char **username, char **password,
 
     // print args not found
     if (*folder == NULL) {
-        printf("Folder not found\n")
+        printf("Folder not found\n");
     }
 
     if (*str_message_num == NULL) {
-        printf("Message not found\n")
+        printf("Message not found\n");
     }
 
     // (optind = extra args not parsed by getopt)
