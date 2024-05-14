12f07e2df38f87663434e24b4a38db7169e12e7f
William Spongberg <wspongberg@student.unimelb.edu.au>
2024-05-14 16:11:32 +1000

    Fixed parse.c + defined constants

--- inc/memory.h
+++ inc/memory.h
@@ -4,9 +4,11 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 void check_memory(void *ptr);
 void free_memory(FILE *stream, int num_ptrs, ...);
+void free_string(char *str);
 
 #endif
